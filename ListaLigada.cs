a7d210ec8efbfba77f98181243104dfe980c9d9c
Yesenia <yeseniamv1995@gmail.com>
2023-06-27 14:14:44 -0700

    Agregar archivos de proyecto.

---
+++ ListaLigada.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+{
+    internal class ListaLigada
+    {
+        Nodo nodoInicial;
+
+
+        public ListaLigada(string valor)
+        {
+            nodoInicial = new Nodo(valor);
+        }
+        public void Agregar(string valor)
+        {
+            Nodo nodoActual = nodoInicial;
+
+            while (nodoActual.Siguiente != null)
+            {
+                nodoActual = nodoActual.Siguiente;
+
+            }
+            
+            nodoActual.Siguiente = new Nodo(valor);
+        }
+
+        public void Relacionar(ListaLigada vertice)
+        {
+
+            Agregar(vertice.nodoInicial.Valor);
+        }
+
+        public int SumaASCII(string entrada)
+        {
+            int suma = 0;
+            foreach (var x in entrada)
+            {
+                suma += (int)x;
+            }
+            return suma;
+        }
+
+        public int GetIndex(string entrada)
+        {
+            
+            int indice = SumaASCII(entrada) % 6;
+            return indice;
+        }
+        /*
+        public void Hasheo(ListaLigada vertice)
+        {
+            
+            int index;
+            byte[] bytes = Encoding.ASCII.GetBytes(vertice.nodoInicial.Valor);
+        }
+        */
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ListaLigada> hash = new List<ListaLigada>();
+
+            //6 espacios
+
+            ListaLigada Posicion0 = new ListaLigada("0");
+            ListaLigada Posicion1 = new ListaLigada("1");
+            ListaLigada Posicion2 = new ListaLigada("2");
+            ListaLigada Posicion3 = new ListaLigada("3");
+            ListaLigada Posicion4 = new ListaLigada("4");
+            ListaLigada Posicion5 = new ListaLigada("5");
+
+
+
+            hash.Add(Posicion0);
+            hash.Add(Posicion1);
+            hash.Add(Posicion2);
+            hash.Add(Posicion3);
+            hash.Add(Posicion4);
+            hash.Add(Posicion5);
+
+        }
+    }
+}
